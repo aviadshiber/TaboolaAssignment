dec406ff27a5188a33e09ffd1cd34b7c020197ea
aviadshiber <aviadshiber@alumni.technion.ac.il>
2022-12-12 22:58:25 +0200

    add log dependency

---
+++ src/main/java/com/taboola/question2/WordsCounter.java
@@ -0,0 +1,102 @@
+package com.taboola.question2;
+
+import lombok.Cleanup;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.LongAdder;
+
+@Slf4j
+public class WordsCounter {
+
+    private final Map<String, LongAdder> wordToCount;
+    private final long timeout;
+    private final TimeUnit timeoutUnit;
+    private final ExecutorService executor;
+    private final Writer writer;
+
+    public WordsCounter() {
+        //initialize WordsCounter with default values:
+        this(10, TimeUnit.NANOSECONDS, new PrintWriter(System.out));
+    }
+
+    public WordsCounter(long timeout, TimeUnit timeoutUnit, Writer writer) {
+        //each thread is reading from IO, hence it is best to have newCachedThreadPool
+        executor = Executors.newCachedThreadPool();
+        this.wordToCount = new ConcurrentHashMap<>();
+        this.timeout = timeout;
+        this.timeoutUnit = timeoutUnit;
+        this.writer = writer;
+    }
+
+    @SneakyThrows
+    public void load(String... files) {
+        @Cleanup val filesStream = Arrays.stream(files).onClose(executor::shutdown);
+
+        filesStream.forEach(filePath -> {
+            CompletableFuture.runAsync(() -> {
+                try {
+                    countWordsInFile(filePath);
+                } catch (Exception e) {
+                    log.error(String.valueOf(e));
+                    executor.shutdown();
+                }
+            }, executor);
+        });
+
+
+    }
+
+    private void countWordsInFile(String filePath) throws IOException {
+        @Cleanup val br = Files.newBufferedReader(Paths.get(filePath), StandardCharsets.UTF_8);
+
+        br.lines().map(l -> l.split("\\s+")).forEach(words -> {
+            for (String word : words) {
+                val key = normalize(word);
+                if (!key.isEmpty()) {
+                    val adder = wordToCount.computeIfAbsent(key, k -> new LongAdder());
+                    adder.increment();
+                }
+            }
+        });
+    }
+
+    private String normalize(String word) {
+        return word.toLowerCase().strip();
+    }
+
+    @SneakyThrows
+    public void displayStatus() {
+        Objects.requireNonNull(executor);
+        log.info("Awaiting for results...");
+        if (!executor.awaitTermination(timeout, timeoutUnit)) {
+            log.warn("Timeout been reached, collecting results until now");
+        }
+        val totalCount = new LongAdder();
+        wordToCount.entrySet().parallelStream().forEach(entry -> {
+            val word = entry.getKey();
+            val count = entry.getValue().sum();
+            totalCount.add(count);
+            try {
+                val out = String.format("%s %d%n", word, count);
+                writer.write(out);
+            } catch (IOException e) {
+                log.error(String.valueOf(e));
+            }
+        });
+        val summary = String.format("** total: %s%n", totalCount.sum());
+        writer.write(summary);
+        writer.flush();
+    }
+}
