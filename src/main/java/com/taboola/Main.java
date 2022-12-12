5d1ec2f9283d7a008999e161c005010fb94cf9af
aviadshiber <aviadshiber@alumni.technion.ac.il>
2022-12-12 23:02:50 +0200

    integrate both solutions

--- src/main/java/com/taboola/Main.java
+++ src/main/java/com/taboola/Main.java
@@ -1,55 +1,17 @@
 package com.taboola;
 
-import com.taboola.question1.BatchLineReader;
-import com.taboola.question1.SevenSegmentDisplayParser;
-import lombok.Cleanup;
+import com.taboola.question1.SolutionOne;
 import lombok.val;
-
-import java.io.*;
 
 
 public class Main {
+    public static void main(String[] args) {
+        val s1 = new SolutionOne();
+        s1.run();
 
-    private static final SevenSegmentDisplayParser parser = new SevenSegmentDisplayParser();
-    final static int batchSize = 4;
-
-    public static void main(String[] args) throws IOException {
-        playSolutionOne();
+//        val s2 = new SolutionTwo();
+//        s2.run();
     }
 
-    private static void playSolutionOne() throws IOException {
-        @Cleanup Writer writer = new PrintWriter(System.out);
-        String[] filePaths = new String[]{
-                "src/main/resources/input_Q1a.txt",
-                "src/main/resources/input_Q1b.txt"
-        };
 
-        for (String path : filePaths) {
-            parse7SegmentFile(path, writer);
-            System.out.println("----------NEW FILE FROM HERE-----------");
-        }
-    }
-
-    private static void parse7SegmentFile(String filePath, Writer writer) throws IOException {
-            @Cleanup val br = new BufferedReader(new FileReader(filePath));
-            val batchReader = new BatchLineReader(br,batchSize);
-            @Cleanup val parsedStream =
-                    batchReader.stream().map(parser)
-                    .onClose(() -> {
-                        try {
-                            writer.flush();
-                        } catch (IOException e) {
-                            System.err.println(e);
-                        }
-                    });
-
-            parsedStream.forEach(parsed ->{
-                try {
-                    writer.write(String.format("%s\n", parsed));
-                } catch (IOException e) {
-                    System.err.println(e);
-                }
-            });
-
-    }
 }
