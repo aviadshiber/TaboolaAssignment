f9f6d4519d81e6cca8e56f38021ee083d35568d2
aviadshiber <aviadshiber@alumni.technion.ac.il>
2022-12-13 12:02:11 +0200

    select which solution to run

--- src/main/java/com/taboola/Main.java
+++ src/main/java/com/taboola/Main.java
@@ -1,16 +1,20 @@
 package com.taboola;
 
 import com.taboola.question1.SolutionOne;
+import com.taboola.question2.SolutionTwo;
 import lombok.val;
+
 
 
 public class Main {
     public static void main(String[] args) {
-        val s1 = new SolutionOne();
-        s1.run();
-
-//        val s2 = new SolutionTwo();
-//        s2.run();
+        if(args.length>0 && "1".equals(args[0])) {
+            val s1 = new SolutionOne();
+            s1.run();
+        }else {
+            val s2 = new SolutionTwo();
+            s2.run();
+        }
     }
 
 
